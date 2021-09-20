cdc56ef3953c3c10410bbc4985c3814ca9e6a4e3
Grégory Planchat <gregory@kiboko.fr>
2021-09-20 10:19:57 +0200

    Update dthe custom plugin in order to use Symfony's DIC

--- src/Plugin/Custom/Builder/Loader.php
+++ src/Plugin/Custom/Builder/Loader.php
@@ -9,9 +9,17 @@ use PhpParser\Node;
 
 final class Loader implements StepBuilderInterface
 {
+    private ?Node\Expr $service;
     private ?Node\Expr $logger;
     private ?Node\Expr $rejection;
     private ?Node\Expr $state;
+
+    public function withService(Node\Expr $service): self
+    {
+        $this->service = $service;
+
+        return $this;
+    }
 
     public function withLogger(Node\Expr $logger): self
     {
@@ -36,137 +44,33 @@ final class Loader implements StepBuilderInterface
 
     public function getNode(): Node
     {
-        $loadStmts = [];
-
-//        foreach ($this->parameters as $key => $parameter){
-//            $loadStmts[]= new Node\Stmt\Expression(
-//                expr: new Node\Expr\MethodCall(
-//                    new Node\Expr\Variable('containerBuilder'),
-//                    'setParameter',
-//                    [
-//                        new Node\Arg(
-//                            new Node\Scalar\String_($key),
-//                        ),
-//                        new Node\Arg(
-//                            new Node\Scalar\String_($parameter),
-//                        ),
-//                    ]
-//                )
-//            );
-//        }
-//        foreach ($this->services as $key => $service) {
-//            $loadStmts[]= new Node\Stmt\Expression(
-//                new Node\Expr\Assign(
-//                    var: new Node\Expr\Variable('service'),
-//                    expr: new Node\Expr\MethodCall(
-//                        new Node\Expr\Variable('containerBuilder'),
-//                        'setParameter',
-//                        [
-//                            new Node\Arg(
-//                                new Node\Scalar\String_($key),
-//                            ),
-//                            new Node\Arg(
-//                                new Node\Scalar\String_($key),
-//                            ),
-//                        ]
-//                    )
-//                ),
-//            );
-//
-//            foreach ($service['arguments'] as $argument) {
-//                $loadStmts[] = new Node\Stmt\Expression(
-//                    new Node\Expr\Assign(
-//                        var: new Node\Expr\Variable('service'),
-//                        expr: new Node\Expr\MethodCall(
-//                            new Node\Expr\Variable('containerBuilder'),
-//                            'addArgument',
-//                            [
-//                                new Node\Arg(
-//                                    new Node\Scalar\String_($argument),
-//                                ),
-//                            ]
-//                        ),
-//                    ),
-//                );
-//            }
-//        }
-//
-//            foreach ($this->services as $key => $service){
-//                $containerBuilder->register($key, $key);
-//                if (array_key_exists('arguments', $service)){
-//                    foreach ($service['arguments'] as $argument){
-//                        $containerBuilder->addArgument($argument);
-//                    }
-//                }
-//
-//            $containerBuilder = new \Symfony\Component\DependencyInjection\ContainerBuilder();
-//            $service = $containerBuilder
-//                ->register('App\Security\Encrypt', 'App\Security\Encrypt')
-//                ->addArgument('$cipher','%cipher%');
-//            $service = $containerBuilder
-//                ->$services = register('App\Loader', 'App\Loader');
-//
-//                $service = $containerBuilder->addArgument(['$client', '$client'])
-//                $service = $containerBuilder->addArgument(['$client', '$client']);
-//
-//            foreach($parameters as $key => $parameter){
-//                $containerBuilder->setParameter('key','parameter');
-//            }
-//
-//            $arguments = [
-//                new Node\Expr\Variable('services'),
-//                new Node\Arg(
-//                    new Node\Scalar\String_($this->use)
-//                )
-//            ];
-        return new Node\Expr\New_(
-            class: new Node\Stmt\Class_(
-                name: null,
+        return new Node\Expr\FuncCall(
+            new Node\Expr\Closure(
                 subNodes: [
+                    'returnType' => new Node\Name\FullyQualified('Kiboko\\Contract\\Pipeline\\LoaderInterface'),
                     'stmts' => [
-                        new Node\Stmt\Property(
-                            flags: Node\Stmt\Class_::MODIFIER_PRIVATE,
-                            props: [
-                                new Node\Stmt\PropertyProperty(
-                                    name: new Node\Identifier('container'),
-                                )
-                            ],
-                            type: new Node\Name\FullyQualified('Symfony\\Component\\DependencyInjection\\ContainerInterface'),
-                        ),
-                        new Node\Stmt\ClassMethod(
-                            name: new Node\Identifier('__construct'),
-                            subNodes: [
-                                'flags' => Node\Stmt\Class_::MODIFIER_PUBLIC,
-                                'stmts' => [
-                                    new Node\Stmt\Expression(
-                                        new Node\Expr\Assign(
-                                            var: new Node\Expr\Variable('containerBuilder'),
-                                            expr: new Node\Expr\New_(
-                                                class: new Node\Name\FullyQualified('Symfony\Component\DependencyInjection\ContainerBuilder')
-                                            ),
-                                        ),
+                        new Node\Stmt\Expression(
+                            new Node\Expr\Include_(
+                                expr: new Node\Expr\BinaryOp\Concat(
+                                    new Node\Expr\ConstFetch(
+                                        new Node\Name\FullyQualified('__DIR__')
                                     ),
-//                                    new Node\Stmt\Expression(
-//                                        new Node\Expr\Assign(
-//                                            new Node\Expr\PropertyFetch(
-//                                                new Node\Expr\Variable('this'),
-//                                                'container'
-//                                            )
-//                                        ),
-//                                    ),
-                                ],
-                            ],
+                                    new Node\Scalar\String_('/container.php'),
+                                ),
+                                type: Node\Expr\Include_::TYPE_REQUIRE,
+                            ),
                         ),
-                        new Node\Stmt\ClassMethod(
-                            name: new Node\Identifier('load'),
-                            subNodes: [
-                                'stmts' => $loadStmts,
-                                'returnType' => new Node\Name\FullyQualified('Generator'),
-                            ],
-                        ),
-                    ],
-                    'implements' => [
-                        new Node\Name\FullyQualified('Kiboko\\Contract\\Pipeline\\LoaderInterface'),
+                        new Node\Stmt\Return_(
+                            expr: new Node\Expr\MethodCall(
+                                var: new Node\Expr\New_(
+                                    class: new Node\Name\FullyQualified('ProjectServiceContainer')
+                                ),
+                                name: new Node\Identifier('get'),
+                                args: [
+                                    new Node\Arg($this->service)
+                                ]
+                            )
+                        )
                     ],
                 ],
             ),
--- src/Plugin/Custom/Factory/Loader.php
+++ src/Plugin/Custom/Factory/Loader.php
@@ -5,6 +5,7 @@ namespace Kiboko\Component\Satellite\Plugin\Custom\Factory;
 
 use Kiboko\Component\Packaging;
 use Kiboko\Contract\Configurator;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\Definition\Processor;
 use Kiboko\Component\Satellite\Plugin\Custom\Configuration;
@@ -12,21 +13,33 @@ use Symfony\Component\Config\Definition\Exception as Symfony;
 use Kiboko\Component\Satellite\Plugin\Custom;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
+use Symfony\Component\DependencyInjection\Parameter;
 use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+use function Kiboko\Component\SatelliteToolbox\Configuration\compileValueWhenExpression;
 
 class Loader implements Configurator\FactoryInterface
 {
     private Processor $processor;
     private ConfigurationInterface $configuration;
-    public function __construct()
-    {
+    private ExpressionLanguage $interpreter;
+
+    public function __construct(
+        ?ExpressionLanguage $interpreter = null
+    ) {
         $this->processor = new Processor();
         $this->configuration = new Configuration();
+        $this->interpreter = $interpreter ?? new ExpressionLanguage();
     }
+
     public function configuration(): ConfigurationInterface
     {
         return $this->configuration;
     }
+
+    /**
+     * @throws Configurator\ConfigurationExceptionInterface
+     */
     public function normalize(array $config): array
     {
         try {
@@ -35,18 +48,21 @@ class Loader implements Configurator\FactoryInterface
             throw new Configurator\InvalidConfigurationException($exception->getMessage(), 0, $exception);
         }
     }
+
     public function validate(array $config): bool
     {
         try {
-            if ($this->normalize($config)) {
-                return true;
-            }
-        } catch (\Exception $exception) {
-            throw new Configurator\InvalidConfigurationException($exception->getMessage(), 0, $exception);
+            $this->processor->processConfiguration($this->configuration, $config);
+
+            return true;
+        } catch (Symfony\InvalidTypeException|Symfony\InvalidConfigurationException $exception) {
+            return false;
         }
-        return false;
     }
 
+    /**
+     * @throws Configurator\ConfigurationExceptionInterface
+     */
     public function compile(array $config): Repository\Loader
     {
         $builder = new Custom\Builder\Loader();
@@ -61,6 +77,9 @@ class Loader implements Configurator\FactoryInterface
                 $container->setParameter($identifier, $parameter);
             }
         }
+
+        $definition = $container->register('logger', LoggerInterface::class);
+        $definition->setFactory('foo::bar');
 
         if (array_key_exists('services', $config)
             && is_array($config['services'])
@@ -92,20 +111,28 @@ class Loader implements Configurator\FactoryInterface
                     }
                 }
 
-//                if (array_key_exists('calls', $service)
-//                    && is_array($service['calls'])
-//                    && count($service['calls']) > 0
-//                ) {
-//                    foreach ($service['calls'] as $key => [$method, $arguments]) {
-//                        $definition->addMethodCall($argument);
-//                    }
-//                }
+                if (array_key_exists('calls', $service)
+                    && is_array($service['calls'])
+                    && count($service['calls']) > 0
+                ) {
+                    foreach ($service['calls'] as $key => [$method, $arguments]) {
+                        $definition->addMethodCall($method, array_map(function ($argument) {
+                            if (preg_match('/^@[^@]/', $argument)) {
+                                return new Reference(\substr($argument, 1));
+                            }
+                            if (preg_match('/^%[^%].*[^%]%$/', $argument)) {
+                                return new Parameter(\substr($argument, 1, -1));
+                            }
+
+                            return $argument;
+                        }, $arguments));
+                    }
+                }
             }
         }
 
-        if (array_key_exists('use', $config)) {
-            $container->getDefinition($config['use'])->setPublic(true);
-        }
+        $container->getDefinition($config['use'])->setPublic(true);
+        $builder->withService(compileValueWhenExpression($this->interpreter, $config['use']));
 
         $repository = new Repository\Loader($builder);
 
