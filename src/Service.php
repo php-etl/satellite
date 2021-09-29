12c39e22e524ce79ee9693e2d2cc21d6c7b38da5
sebprt <sebastien.parrat@orange.fr>
2021-09-29 17:36:40 +0200

    Updated MemoryState with the new StateInterface, and updated the RabbitMQ config

--- src/Service.php
+++ src/Service.php
@@ -6,7 +6,6 @@ namespace Kiboko\Component\Satellite;
 
 use Kiboko\Component\Packaging;
 use Kiboko\Component\Satellite;
-use Kiboko\Component\Satellite\Builder\Workflow\PipelineBuilder;
 use Kiboko\Contract\Configurator;
 use Kiboko\Plugin\CSV;
 use Kiboko\Plugin\Akeneo;
@@ -167,8 +166,8 @@ final class Service implements Configurator\FactoryInterface
         $interpreter = new Satellite\ExpressionLanguage\ExpressionLanguage();
 
         $repository->addPackages(
-            'php-etl/pipeline-contracts:~0.2.0@dev',
-            'php-etl/pipeline:~0.3.0@dev',
+            'php-etl/pipeline-contracts:~0.3.0@dev',
+            'php-etl/pipeline:~0.4.0@dev',
             'psr/log:^1.1',
             'monolog/monolog',
             'symfony/console:^5.2',
@@ -246,7 +245,7 @@ final class Service implements Configurator\FactoryInterface
                 $clone = clone $interpreter;
                 (new Satellite\Pipeline\ConfigurationApplier('csv', new CSV\Service(clone $clone), $clone))
                     ->withPackages(
-                        'php-etl/pipeline-contracts:~0.2.0@dev',
+                        'php-etl/pipeline-contracts:~0.3.0@dev',
                         'php-etl/bucket-contracts:~0.1.0@dev',
                         'php-etl/bucket:~0.2.0@dev',
                         'php-etl/csv-flow:~0.2.0@dev',
@@ -282,7 +281,7 @@ final class Service implements Configurator\FactoryInterface
                 $clone = clone $interpreter;
                 (new Satellite\Pipeline\ConfigurationApplier('fastmap', new FastMap\Service(clone $clone), $clone))
                     ->withPackages(
-                        'php-etl/pipeline-contracts:~0.2.0@dev',
+                        'php-etl/pipeline-contracts:~0.3.0@dev',
                         'php-etl/bucket-contracts:~0.1.0@dev',
                         'php-etl/bucket:~0.2.0@dev',
                         'php-etl/fast-map:~0.2.0@dev',
