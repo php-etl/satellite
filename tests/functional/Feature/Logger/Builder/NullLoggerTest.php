c1b6fb8d9a2172a0b8ce8270c3dfcec79a9c2c23
sebprt <sebastien@kiboko.fr>
2022-05-18 17:06:58 +0200

    Made class abstract

--- tests/functional/Feature/Logger/Builder/NullLoggerTest.php
+++ tests/functional/Feature/Logger/Builder/NullLoggerTest.php
@@ -7,7 +7,7 @@ use Kiboko\Component\Satellite\Feature\Logger\Builder;
 use PHPUnit\Framework\TestCase;
 use Vfs\FileSystem;
 
-final class NullLoggerTest extends TestCase
+abstract class NullLoggerTest extends TestCase
 {
     use PipelineBuilderAssertTrait;
 
