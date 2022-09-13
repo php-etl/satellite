a8bc5e1ade6f871bc40c22a8b0d3d4ceab38a00f
sebprt <sebastien@kiboko.fr>
2022-09-13 11:36:55 +0200

    Enabled expression_languages in config

--- src/Plugin/Custom/Service.php
+++ src/Plugin/Custom/Service.php
@@ -71,23 +71,25 @@ final class Service implements Configurator\PipelinePluginInterface
      */
     public function compile(array $config): Configurator\RepositoryInterface
     {
+        $interpreter = clone $this->interpreter;
+
         if (\array_key_exists('expression_language', $config)
             && \is_array($config['expression_language'])
             && \count($config['expression_language'])
         ) {
             foreach ($config['expression_language'] as $provider) {
-                $this->interpreter->registerProvider(new $provider());
+                $interpreter->registerProvider(new $provider());
             }
         }
 
         if (\array_key_exists('extractor', $config)) {
-            return (new Custom\Factory\Extractor($this->interpreter))->compile($config['extractor']);
+            return (new Custom\Factory\Extractor($interpreter))->compile($config['extractor']);
         }
         if (\array_key_exists('transformer', $config)) {
-            return (new Custom\Factory\Transformer($this->interpreter))->compile($config['transformer']);
+            return (new Custom\Factory\Transformer($interpreter))->compile($config['transformer']);
         }
         if (\array_key_exists('loader', $config)) {
-            return (new Custom\Factory\Loader($this->interpreter))->compile($config['loader']);
+            return (new Custom\Factory\Loader($interpreter))->compile($config['loader']);
         }
 
         throw new \RuntimeException('No possible pipeline step, expecting "extractor", "transformer" or "loader".');
