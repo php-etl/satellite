a2ec15df4b4bc168abe7614b9a4bd0f01f9b2844
Grégory Planchat <gregory@kiboko.fr>
2023-04-18 16:18:27 +0200

    Updated the compiled code packages dependencies

--- rector.php
+++ rector.php
@@ -4,25 +4,20 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\Symfony\Set\SymfonyLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/examples',
-        __DIR__ . '/project-fpm',
         __DIR__ . '/src',
-        __DIR__ . '/tests',
     ]);
 
-    // register a single rule
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
 
-    // define sets of rules
-        $rectorConfig->sets([
-            LevelSetList::UP_TO_PHP_82,
-            PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
-            SymfonyLevelSetList::UP_TO_SYMFONY_60,
-        ]);
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_82,
+        PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
+        SymfonyLevelSetList::UP_TO_SYMFONY_60,
+    ]);
 };
