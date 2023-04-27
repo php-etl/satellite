206d4ec51c1caff0e8b8b8808a172f812daccaf4
sebprt <sebastien@kiboko.fr>
2023-04-27 17:39:28 +0200

    Fixed normalization in the custom plugin

--- src/Plugin/Custom/Configuration.php
+++ src/Plugin/Custom/Configuration.php
@@ -31,6 +31,16 @@ final class Configuration implements PluginConfigurationInterface
                 ->ifTrue(fn (array $value) => \array_key_exists('loader', $value) && \array_key_exists('transformer', $value))
                 ->thenInvalid('Your configuration should either contain the "loader" or the "transformer" key, not both.')
             ->end()
+            ->beforeNormalization()
+                ->always(function (array $value) {
+                    if (\array_key_exists('expression_language', $value) && \count($value['expression_language']) <= 0)
+                    {
+                        unset($value['expression_language']);
+                    }
+
+                    return $value;
+                })
+            ->end()
             ->children()
                 ->arrayNode('expression_language')
                     ->scalarPrototype()->end()
