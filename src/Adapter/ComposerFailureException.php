67afdaf4da1f6ec0a13a4a0aa68d4297d52c662e
clementzarch <clement@kiboko.fr>
2023-05-18 09:28:24 +0200

    clear vendors when composer.json is not valid, allow php-http/discovery plugin, tell the user when there is no pipeline.php or workflow.php

---
+++ src/Adapter/ComposerFailureException.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kiboko\Component\Satellite\Adapter;
+
+final class ComposerFailureException extends \RuntimeException
+{
+    public function __construct(private readonly string $command = '', string $message = '', int $code = 0, ?\Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getCommand(): string
+    {
+        return $this->command;
+    }
+}
