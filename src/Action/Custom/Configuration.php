252325d76e6abb86da8ad398a7866f6b59f8b999
sebprt <sebastien@kiboko.fr>
2023-06-14 16:24:22 +0200

    Integration of custom actions + automatic management of actions in the .gyro file

---
+++ src/Action/Custom/Configuration.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kiboko\Component\Satellite\Action\Custom;
+
+use Kiboko\Component\Satellite\DependencyInjection\Configuration\ServicesConfiguration;
+use Kiboko\Contract\Configurator\ActionConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use function Kiboko\Component\SatelliteToolbox\Configuration\asExpression;
+use function Kiboko\Component\SatelliteToolbox\Configuration\isExpression;
+
+final class Configuration implements ActionConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $builder = new TreeBuilder('custom');
+        /* @phpstan-ignore-next-line */
+        $builder->getRootNode()
+            ->children()
+                ->arrayNode('expression_language')
+                    ->scalarPrototype()->end()
+                ->end()
+                ->append((new ServicesConfiguration())->getConfigTreeBuilder()->getRootNode())
+                    ->scalarNode('use')
+                        ->isRequired()
+                    ->end()
+                    ->arrayNode('parameters')
+                        ->useAttributeAsKey('keyparam')
+                        ->scalarPrototype()
+                        ->cannotBeEmpty()
+                        ->validate()
+                            ->ifTrue(isExpression())
+                            ->then(asExpression())
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+        ;
+
+        return $builder;
+    }
+}
--- src/Action/Custom/Service.php
+++ src/Action/Custom/Service.php
@@ -2,31 +2,28 @@
 
 declare(strict_types=1);
 
-namespace Kiboko\Component\Satellite\Plugin\SFTP;
+namespace Kiboko\Component\Satellite\Action\Custom;
 
+use Kiboko\Component\Satellite\Action\Custom;
 use Kiboko\Component\Satellite\ExpressionLanguage as Satellite;
-use Kiboko\Component\Satellite\Plugin\SFTP\Factory\Repository\Repository;
 use Kiboko\Contract\Configurator;
 use Symfony\Component\Config\Definition\Exception as Symfony;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
 
-#[Configurator\Pipeline(
-    name: 'sftp',
-    dependencies: ['ext-ssh2'],
-    steps: [
-        new Configurator\Pipeline\StepLoader(),
-    ],
-)] final readonly class Service implements Configurator\PipelinePluginInterface
+#[Configurator\Action(
+    name: 'custom',
+)]
+final readonly class Service implements Configurator\ActionInterface
 {
     private Processor $processor;
-    private Configurator\PluginConfigurationInterface $configuration;
+    private Configurator\ActionConfigurationInterface $configuration;
 
     public function __construct(
         private ExpressionLanguage $interpreter = new Satellite\ExpressionLanguage()
     ) {
         $this->processor = new Processor();
-        $this->configuration = new Configuration();
+        $this->configuration = new Custom\Configuration();
     }
 
     public function interpreter(): ExpressionLanguage
@@ -34,7 +31,7 @@ use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
         return $this->interpreter;
     }
 
-    public function configuration(): Configurator\PluginConfigurationInterface
+    public function configuration(): Configurator\ActionConfigurationInterface
     {
         return $this->configuration;
     }
@@ -67,33 +64,18 @@ use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
      */
     public function compile(array $config): Configurator\RepositoryInterface
     {
+        $interpreter = clone $this->interpreter;
+
         if (\array_key_exists('expression_language', $config)
             && \is_array($config['expression_language'])
             && \count($config['expression_language'])
         ) {
             foreach ($config['expression_language'] as $provider) {
-                $this->interpreter->registerProvider(new $provider());
+                $interpreter->registerProvider(new $provider());
             }
         }
 
-        if (\array_key_exists('extractor', $config)) {
-            $factory = new Factory\Extractor($this->interpreter);
-
-            $loader = $factory->compile($config);
-            $builder = $loader->getBuilder();
-
-            return new Repository($builder);
-        }
-
-        if (\array_key_exists('loader', $config)) {
-            $loaderFactory = new Factory\Loader($this->interpreter);
-
-            $loaderCompilation = $loaderFactory->compile($config);
-            $loaderBuilder = $loaderCompilation->getBuilder();
-
-            return new Repository($loaderBuilder);
-        }
-
-        throw new \RuntimeException('No suitable build with the provided configuration.');
+        $actionFactory = new Custom\Factory\Action($this->interpreter);
+        return $actionFactory->compile($config);
     }
 }
