105d446457dcda255ef2cf439eab46e9a37a548a
Sébastien Parrat <47692802+sebprt@users.noreply.github.com>
2024-01-16 11:09:57 +0100

    Applied suggestion
    Co-authored-by: Grégory PLANCHAT <gplanchat@users.noreply.github.com>

--- src/DependencyInjection/SatelliteDependencyInjection.php
+++ src/DependencyInjection/SatelliteDependencyInjection.php
@@ -93,7 +93,7 @@ final class SatelliteDependencyInjection
                     $definition->setFactory([$service['factory']['class'], $service['factory']['method']]);
                 }
 
-                $definition->setPublic($config['use'] === $identifier ? true : $service['public']);
+                $definition->setPublic($config['use'] === $identifier || $service['public']);
             }
         }
 
