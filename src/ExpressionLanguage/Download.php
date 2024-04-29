9679a27401a16cbc045b219294c7eb9f338851a8
Jonathan <jonathan@kiboko.fr>
2024-04-29 12:34:19 +0200

    Add expression download to return a ressource from a path or url

---
+++ src/ExpressionLanguage/Download.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kiboko\Component\Satellite\ExpressionLanguage;
+
+use PhpSpec\Locator\Resource;
+use Symfony\Component\ExpressionLanguage\ExpressionFunction;
+
+final class Download extends ExpressionFunction
+{
+    public function __construct(string $name)
+    {
+        parent::__construct(
+            $name,
+            function (string $value, string $context = ''): string {
+                $pattern = <<<'PHP'
+                    (function () use($input) {
+                        $resource = \fopen(%s %s, 'r');
+                        if ($resource === false) {
+                            throw new \RuntimeException('Could not open file.');
+                        }
+                        return $resource;
+                    })()
+                    PHP;
+
+                return sprintf($pattern, $value, $context);
+            },
+            function (string $value, string $context = '') {
+                $resource = fopen($context . $value, 'r');
+                if ($resource === false) {
+                    throw new \RuntimeException('Could not open file.');
+                }
+                return $resource;
+            },
+        );
+    }
+}
